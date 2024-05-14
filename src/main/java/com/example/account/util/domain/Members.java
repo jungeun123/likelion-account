6d9619f7786cdc389f1aeb83288cce27639de2db
jungeun123 <147065885+jungeun123@users.noreply.github.com>
2024-05-14 16:29:18 +0900

    :sparkles: Entity 계층 작성

---
+++ src/main/java/com/example/account/util/domain/Members.java
@@ -0,0 +1,34 @@
+package com.example.account.util.domain;
+
+import com.example.account.util.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "MEMBERS")
+@Getter // Setter x
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Members extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+}
